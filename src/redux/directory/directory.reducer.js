5b0950f197fd7631e7c7dd4c7f225a87fc26bbe5
NikQuila <nicopirozzi1@gmail.com>
2021-02-09 11:53:55 -0300

    (dale)

--- src/redux/directory/directory.reducer.js
+++ src/redux/directory/directory.reducer.js
@@ -1,36 +1,35 @@
 const INITIAL_STATE = {
     sections: [
         {
-          title: "Mario",
-          imageUrl: "https://wallpaperaccess.com/full/514007.jpg",
+          title: "Hats",
+          imageUrl: "http://images.unsplash.com/photo-1514327605112-b887c0e61c0a?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MXwxMjA3fDB8MXxhbGx8fHx8fHx8fA&ixlib=rb-1.2.1&q=80&w=1080",
           id: 1,
           linkUrl: "shop/hats",
         },
         {
-          title: "Sonic",
+          title: "Jackets",
           imageUrl:
-            "https://i.pinimg.com/originals/fd/de/c6/fddec6b2a0e806216720bd6dba01a604.png",
+            "https://images-na.ssl-images-amazon.com/images/I/51ylk6F0rfL._AC_UY445_.jpg",
           id: 2,
           linkUrl: "shop/jackets",
         },
         {
-          title: "Link",
+          title: "Sneakers",
           imageUrl:
-            "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/bf3e9148-9803-4bc7-bbf4-8a7bdee78225/d5eu25x-7a386817-da4d-40ab-833b-d03c986c6878.png/v1/fill/w_900,h_507,q_80,strp/link_desktop_background_by_theamazinginfernape_d5eu25x-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3siaGVpZ2h0IjoiPD01MDciLCJwYXRoIjoiXC9mXC9iZjNlOTE0OC05ODAzLTRiYzctYmJmNC04YTdiZGVlNzgyMjVcL2Q1ZXUyNXgtN2EzODY4MTctZGE0ZC00MGFiLTgzM2ItZDAzYzk4NmM2ODc4LnBuZyIsIndpZHRoIjoiPD05MDAifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6aW1hZ2Uub3BlcmF0aW9ucyJdfQ.lFGHm3TuBLliTXvXzlfU35Xi-N-OPw187LIB6exRpik",
+            "https://sneakerize.gr/wp-content/uploads/2018/12/Sneaker-Feature-.jpg",
           id: 3,
           linkUrl: "",
         },
         {
-          title: "Pokemon",
-          imageUrl:
-            "https://i.pinimg.com/originals/78/0c/17/780c1775c94f0ca93807bd0a6d18535f.jpg",
+          title: "Mens",
+          imageUrl: "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.creativefabrica.com%2Fes%2Fproduct%2Fbusiness-men-logo%2F&psig=AOvVaw34O59ClJIWuOtU7ttIi9K2&ust=1612968682894000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICmpN6G3e4CFQAAAAAdAAAAABAD",
           size: "large",
           id: 4,
           linkUrl: "",
         },
         {
-          title: "Naruto",
-          imageUrl: "https://cdn.wallpapersafari.com/78/32/cxXVUf.jpg",
+          title: "Womens",
+          imageUrl: "https://i.pinimg.com/originals/6a/88/98/6a889889f8f8bd8962818dfe13fcd4c8.png",
           size: "large",
           id: 5,
           linkUrl: "",
